b2bd36fd8e732e5c5366fcfa79dec99a0a9c4c5a
Stefan Boos <sboos@igus.net>
2023-12-18 17:43:09 +0100

    feat: we can rate single frames
    Co-authored-by: Niklas Rueber <nrueber@igus.net>
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>
    Co-authored-by: Holger Janßen-Kroll <hjanssenkroll@igus.net>

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import 'mocha';
 
-it("failing frame ist recognized", () => {
+it("failing frame is recognized", () => {
     expect( 0 ).to.be.equal(RateSingleFrame('--'));
 })
 
@@ -13,13 +13,36 @@ it("failing game is recognized", () => {
     expect( 0 ).to.be.equal(SplitGameIntoFrames('-- -- -- -- -- -- -- -- -- --').reduce((accumulator, frame) => accumulator + RateSingleFrame(frame), 0));
 })
 
+it("one frame is 9", () => {
+    expect( 9 ).to.be.equal(RateSingleFrame('-9'));
+})
+
+it("other part of frame is 9", () => {
+    expect( 9 ).to.be.equal(RateSingleFrame('9-'));
+})
+
+it("left and right get added", () => {
+    expect( 9 ).to.be.equal(RateSingleFrame('81'));
+})
+
 function SplitGameIntoFrames(game: string): string[] {
     return game.split(" ");
 }
 
 function RateSingleFrame(frame: string): any {
+    const valueOfSecondThrow = Number(frame[1]);
+    const valueOfFirstThrow = Number(frame[0]);
+    const firstThrowIsHit = !isNaN(valueOfFirstThrow);
+    const secondThrowIsHit = !isNaN(valueOfSecondThrow);
+    
     if (frame == '--' ) {
-        return 0
+        return 0;
+    } else if (firstThrowIsHit && secondThrowIsHit ) {
+        return valueOfFirstThrow + valueOfSecondThrow;
+    } else if(secondThrowIsHit){
+        return valueOfSecondThrow;
+    } else if(firstThrowIsHit){
+        return valueOfFirstThrow;
     }
     return -99;
 }
