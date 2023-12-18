7a407cb1f4915043aa4fb9c9040789179e5f0da9
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-18 18:11:54 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:tests/index.spec.ts

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import 'mocha';
 
+
 it("failing frame is recognized", () => {
     expect( 0 ).to.be.equal(RateSingleFrame('--'));
 })
@@ -9,22 +10,18 @@ it("splits a line of 10 frames", () => {
     expect( ["--","--","--","--","--","--","--","--","--","--" ] ).to.be.deep.equal(SplitGameIntoFrames('-- -- -- -- -- -- -- -- -- --'));
 })
 
+// todo: parameterize test
 it("sums up frames", () => {
     expect( 9 ).to.be.equal(EvaluateGame('-9 -- -- -- -- -- -- -- -- --'));
-})
-
-it("sums up frames 2", () => {
     expect( 18 ).to.be.equal(EvaluateGame('-9 -9 -- -- -- -- -- -- -- --'));
     expect( 90 ).to.be.equal(EvaluateGame('-9 -9 9- -9 -9 9- 9- 81 18 27'));
+    expect( 81 ).to.be.equal(EvaluateGame('-9 -9 9- 12 34 53 9- 81 18 27'));
+    expect( 0 ).to.be.equal(EvaluateGame('-- -- -- -- -- -- -- -- -- --'));
 })
 
 //it.each([[18, '-9 -9 -- -- -- -- -- -- -- --']])("sums up frames 3", (expected :number, game :string) => {
 //    expect( expected ).to.be.equal(SplitGameIntoFrames(game).reduce((accumulator, frame) => accumulator + RateSingleFrame(frame), 0));
 //})
-
-it("failing game is recognized", () => {
-    expect( 0 ).to.be.equal(EvaluateGame('-- -- -- -- -- -- -- -- -- --'));
-})
 
 it("one frame is 9", () => {
     expect( 9 ).to.be.equal(RateSingleFrame('-9'));
